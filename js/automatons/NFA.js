6d0f8188cac45b5dd8f79a78bc810f934e2cb6e9
TilenOkretic <tilen.okretic@gmail.com>
2021-05-01 01:58:44 +0200

    FIRST STEP of NKA somehow i finally got transitions to work somewhat correctly now on to the next step

--- js/automatons/NFA.js
+++ js/automatons/NFA.js
@@ -5,16 +5,37 @@ class NFA extends AutomatonBase {
     }
 
     process_string(str) {
+        return this.process_transition(this.transitions[this.start.name], str);
+    }
 
-        if (!this.transitions[this.start.name]) {
+    // abc
+    process_transition(transition, str) {
+        if(!str){
             return false;
         }
-        
-        let next     = this.transitions[this.start];
-        
-        let next_arr = next.rules[str[0]];
-        console.log(next_arr);
+        let next_arr = transition.rules[str[0]];
 
-        // for(let i = 0; i <)
+        if(!next_arr){
+            return false;
+        }
+
+        console.log(str, next_arr);
+
+        for (let i = 0; i < next_arr.length; i++) {
+            let next = next_arr[i];
+            let next_transition = this.transitions[next.name];
+            console.log(str[0],next_transition);
+            if(next_transition){
+                if(this.process_transition(next_transition, str.slice(1))){
+                    return true;
+                }
+            }
+
+            if(next.finish && str.length ==1){
+                return true;
+            }
+        }
+
+        return false;
     }
 }
